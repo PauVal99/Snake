774f1afb3c1d02e66d134cc1456ee85002789e2c
Pau <pau.val.tubau99@gmail.com>
2025-09-19 14:03:01 +0200

    feat: decoupled logic from visuals

--- src/snake.c
+++ src/snake.c
@@ -1,4 +1,9 @@
 #include "raylib.h"
+#include "raymath.h"
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define ONE 1
 
 #define HEAD 0
 #define ROWS 20
@@ -15,7 +20,7 @@ typedef enum
 typedef struct SnakeBody
 {
     bool apple;
-    Rectangle rectangle;
+    Vector2 position;
 } SnakeBody;
 
 typedef struct Snake
@@ -27,46 +32,41 @@ typedef struct Snake
 
 typedef struct Apple
 {
-    Rectangle rectangle;
+    Vector2 position;
 } Apple;
 
-Snake CreateSnake(int initBodySize, int maxBodySize, int tileSize)
+Snake CreateSnake(int initBodySize, int maxBodySize)
 {
     Snake snake = {RIGHT, initBodySize};
 
     for (int i = 0; i < initBodySize; i++)
     {
         snake.body[i].apple = false;
-        snake.body[i].rectangle.x = (10 * tileSize) - (i * tileSize);
-        snake.body[i].rectangle.y = 10 * tileSize;
-        snake.body[i].rectangle.width = tileSize;
-        snake.body[i].rectangle.height = tileSize;
+        snake.body[i].position.x = 10 - i;
+        snake.body[i].position.y = 10;
     }
     for (int i = initBodySize; i < maxBodySize; i++)
     {
         snake.body[i].apple = false;
-        snake.body[i].rectangle.x = 0;
-        snake.body[i].rectangle.y = 0;
-        snake.body[i].rectangle.width = 0;
-        snake.body[i].rectangle.height = 0;
+        snake.body[i].position.x = 0;
+        snake.body[i].position.y = 0;
     }
     return snake;
 }
 
-Vector2 GetValidApplePosition(Snake snake, int tileSize)
+Vector2 GetValidApplePosition(Snake snake)
 {
-    Vector2 position = {GetRandomValue(0, ROWS - 1) * tileSize, GetRandomValue(0, COLUMNS - 1) * tileSize};
+    Vector2 position = {GetRandomValue(0, ROWS - 1), GetRandomValue(0, COLUMNS - 1)};
     for (int i = 0; i < snake.bodySize; i++)
-        if ((position.x == snake.body[i].rectangle.x) && (position.y == snake.body[i].rectangle.y))
-            return GetValidApplePosition(snake, tileSize);
+        if ((position.x == snake.body[i].position.x) && (position.y == snake.body[i].position.y))
+            return GetValidApplePosition(snake);
     return position;
 }
 
-Apple CreateApple(Snake snake, int tileSize)
+Apple CreateApple(Snake snake)
 {
-    Vector2 position = GetValidApplePosition(snake, tileSize);
-    Apple apple = {(Rectangle){position.x, position.y, tileSize, tileSize}};
-    return apple;
+    Vector2 position = GetValidApplePosition(snake);
+    return (Apple){position};
 }
 
 int main(void)
@@ -74,18 +74,17 @@ int main(void)
     const int screenWidth = 720;
     const int screenHeight = 720;
 
-    const int tileSize = screenWidth / ROWS;
+    const float tileSize = MIN(screenWidth, screenHeight) / ROWS;
 
     const int initBodySize = 3;
     const int maxBodySize = ROWS * COLUMNS;
 
-    const int stepSize = tileSize;
     const float stepTime = 0.25f;
 
     InitWindow(screenWidth, screenHeight, "Snake 🐍");
 
-    Snake snake = CreateSnake(initBodySize, maxBodySize, tileSize);
-    Apple apple = CreateApple(snake, tileSize);
+    Snake snake = CreateSnake(initBodySize, maxBodySize);
+    Apple apple = CreateApple(snake);
 
     float timer = 0.0f;
     int score = 0;
@@ -106,53 +105,49 @@ int main(void)
                     snake.body[i - 1].apple = false;
 
                     snake.body[snake.bodySize].apple = false;
-                    snake.body[snake.bodySize].rectangle.x = snake.body[i].rectangle.x;
-                    snake.body[snake.bodySize].rectangle.y = snake.body[i].rectangle.y;
-                    snake.body[snake.bodySize].rectangle.width = tileSize;
-                    snake.body[snake.bodySize].rectangle.height = tileSize;
+                    snake.body[snake.bodySize].position.x = snake.body[i].position.x;
+                    snake.body[snake.bodySize].position.y = snake.body[i].position.y;
                     snake.bodySize++;
                 }
                 snake.body[i].apple = snake.body[i - 1].apple;
                 snake.body[i - 1].apple = false;
-                snake.body[i].rectangle.x = snake.body[i - 1].rectangle.x;
-                snake.body[i].rectangle.y = snake.body[i - 1].rectangle.y;
+                snake.body[i].position.x = snake.body[i - 1].position.x;
+                snake.body[i].position.y = snake.body[i - 1].position.y;
             }
             switch (snake.direction)
             {
             case UP:
-                snake.body[HEAD].rectangle.y -= stepSize;
-                if (snake.body[HEAD].rectangle.y < 0)
-                    snake.body[HEAD].rectangle.y = (ROWS - 1) * tileSize;
+                snake.body[HEAD].position.y -= ONE;
+                if (snake.body[HEAD].position.y < 0)
+                    snake.body[HEAD].position.y = ROWS - 1;
                 break;
             case RIGHT:
-                snake.body[HEAD].rectangle.x += stepSize;
-                if (snake.body[HEAD].rectangle.x > (COLUMNS * tileSize))
-                    snake.body[HEAD].rectangle.x = 0;
+                snake.body[HEAD].position.x += ONE;
+                if (snake.body[HEAD].position.x > COLUMNS)
+                    snake.body[HEAD].position.x = 0;
                 break;
             case DOWN:
-                snake.body[HEAD].rectangle.y += stepSize;
-                if (snake.body[HEAD].rectangle.y > (ROWS * tileSize))
-                    snake.body[HEAD].rectangle.y = 0;
+                snake.body[HEAD].position.y += ONE;
+                if (snake.body[HEAD].position.y > ROWS)
+                    snake.body[HEAD].position.y = 0;
                 break;
             case LEFT:
-                snake.body[HEAD].rectangle.x -= stepSize;
-                if (snake.body[HEAD].rectangle.x < 0)
-                    snake.body[HEAD].rectangle.x = (COLUMNS - 1) * tileSize;
+                snake.body[HEAD].position.x -= ONE;
+                if (snake.body[HEAD].position.x < 0)
+                    snake.body[HEAD].position.x = COLUMNS - 1;
                 break;
             default:
-                snake.body[HEAD].rectangle.x += stepSize;
-                if (snake.body[HEAD].rectangle.x > (COLUMNS * tileSize))
-                    snake.body[HEAD].rectangle.x = 0;
+                snake.body[HEAD].position.x += ONE;
+                if (snake.body[HEAD].position.x > COLUMNS)
+                    snake.body[HEAD].position.x = 0;
                 break;
             }
         }
 
-        if ((snake.body[HEAD].rectangle.x == apple.rectangle.x) && (snake.body[HEAD].rectangle.y == apple.rectangle.y))
+        if ((snake.body[HEAD].position.x == apple.position.x) && (snake.body[HEAD].position.y == apple.position.y))
         {
             snake.body[HEAD].apple = true;
-            Vector2 newApplePosition = GetValidApplePosition(snake, tileSize);
-            apple.rectangle.x = newApplePosition.x;
-            apple.rectangle.y = newApplePosition.y;
+            apple.position = GetValidApplePosition(snake);
             score++;
         }
 
@@ -174,18 +169,19 @@ int main(void)
         for (int i = 0; i < screenWidth / tileSize; i++)
             DrawLine(0, i * tileSize, screenWidth, i * tileSize, RED);
 
-        DrawRectangleRec(apple.rectangle, RED);
+        Vector2 size = (Vector2){tileSize, tileSize};
+        DrawRectangleV(Vector2Scale(apple.position, tileSize), size, RED);
         for (int i = 0; i < snake.bodySize; i++)
         {
             if (snake.body[i].apple)
-                DrawRectangleRec(snake.body[i].rectangle, BROWN);
+                DrawRectangleV(Vector2Scale(snake.body[i].position, tileSize), size, BROWN);
             else
-                DrawRectangleRec(snake.body[i].rectangle, BLACK);
+                DrawRectangleV(Vector2Scale(snake.body[i].position, tileSize), size, BLACK);
         }
 
         DrawText(TextFormat("Score: %i", score), 10, 10, 20, BLACK);
         DrawText(TextFormat("Body size: %i", snake.bodySize), 10, 40, 20, BLACK);
-        DrawText(TextFormat("Head: [%i, %i]", (int)(snake.body[HEAD].rectangle.x / tileSize), (int)(snake.body[HEAD].rectangle.y / tileSize)), 10, 70, 20, BLACK);
+        DrawText(TextFormat("Head: [%i, %i]", (int)(snake.body[HEAD].position.x), (int)(snake.body[HEAD].position.y)), 10, 70, 20, BLACK);
 
         EndDrawing();
     }
